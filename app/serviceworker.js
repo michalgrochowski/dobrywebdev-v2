7ac4c18c171c221a57cc1baddbfb062fea3f3bf8
Michał Grochowski <mic.grochowski@gmail.com>
2018-03-14 12:24:42 +0100

    v4.7.6
    Some small changes to serviceworker and gulpfile, also switched from 4 spaces to 2 spaces in most files

--- app/serviceworker.js
+++ app/serviceworker.js
@@ -1,103 +1,108 @@
-const CACHE_NAME = "dobrywebdev-1520935802273";
+const CACHE_STATIC_NAME = "dobrywebdev";
+const CACHE_DYNAMIC_NAME = "dobrywebdev-1521013165219";
 const URLS_TO_CACHE  = [
-    "/",
-    "/index.html",
-    "/errors/404.html",
-    "/manifest.json",
-    "https://fonts.googleapis.com/css?family=Poppins:400,700&amp;subset=latin-ext",
-    "/css/main.min.css",
-    "/js/main.min.js",
-    "/fonts/devicons.eot",
-    "/fonts/devicons.svg",
-    "/fonts/devicons.ttf",
-    "/fonts/devicons.woff",
-    "/font/fontello.eot",
-    "/font/fontello.svg",
-    "/font/fontello.ttf",
-    "/font/fontello.woff",
-    "/font/fontello.woff2",
-    "/json/content-eng.json",
-    "/json/content-pl.json",
-    "/img/playground.png",
-    "/img/agency.png",
-    "/img/cv.png",
-    "/img/dobrywebdev.png",
-    "/img/dobrywebdevfb.png",
-    "/img/form.png",
-    "/img/interior.png",
-    "/img/interior1.png",
-    "/img/logo-dark.png",
-    "/img/logo-light.png",
-    "/img/simpleweather.png",
-    "/img/sleszynski.png",
-    "/img/todo.png",
-    "/img/webdev37.png",
-    "/android-chrome-36x36.png",
-    "/android-chrome-48x48.png",
-    "/android-chrome-72x72.png",
-    "/android-chrome-96x96.png",
-    "/android-chrome-144x144.png",
-    "/android-chrome-192x192.png",
-    "/android-chrome-256x256.png",
-    "/android-chrome-512x512.png",
-    "/apple-touch-icon.png",
-    "/apple-touch-icon-60x60.png",
-    "/apple-touch-icon-76x76.png",
-    "/apple-touch-icon-120x120.png",
-    "/apple-touch-icon-152x152.png",
-    "/apple-touch-icon-180x180.png",
-    "/browserconfig.xml",
-    "/favicon-16x16.png",
-    "/favicon-32x32.png",
-    "/mstile-150x150.png",
-    "/safari-pinned-tab.svg"
+  "/",
+  "/index.html",
+  "/errors/404.html",
+  "/manifest.json",
+  "https://fonts.googleapis.com/css?family=Poppins:400,700&amp;subset=latin-ext",
+  "/css/main.min.css",
+  "/js/main.min.js",
+  "/fonts/devicons.eot",
+  "/fonts/devicons.svg",
+  "/fonts/devicons.ttf",
+  "/fonts/devicons.woff",
+  "/font/fontello.eot",
+  "/font/fontello.svg",
+  "/font/fontello.ttf",
+  "/font/fontello.woff",
+  "/font/fontello.woff2",
+  "/json/content-eng.json",
+  "/json/content-pl.json",
+  "/img/playground.png",
+  "/img/agency.png",
+  "/img/dobrywebdev.png",
+  "/img/dobrywebdevfb.png",
+  "/img/interior.png",
+  "/img/interior1.png",
+  "/img/logo-dark.png",
+  "/img/logo-light.png",
+  "/img/simpleweather.png",
+  "/img/sleszynski.png",
+  "/img/todo.png",
+  "/img/webdev37.png",
+  "/android-chrome-36x36.png",
+  "/android-chrome-48x48.png",
+  "/android-chrome-72x72.png",
+  "/android-chrome-96x96.png",
+  "/android-chrome-144x144.png",
+  "/android-chrome-192x192.png",
+  "/android-chrome-256x256.png",
+  "/android-chrome-512x512.png",
+  "/apple-touch-icon.png",
+  "/apple-touch-icon-60x60.png",
+  "/apple-touch-icon-76x76.png",
+  "/apple-touch-icon-120x120.png",
+  "/apple-touch-icon-152x152.png",
+  "/apple-touch-icon-180x180.png",
+  "/browserconfig.xml",
+  "/favicon-16x16.png",
+  "/favicon-32x32.png",
+  "/mstile-150x150.png",
+  "/safari-pinned-tab.svg"
 ];
 
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(function(cache) {
-            console.log('Opened cache');
-            return cache.addAll(URLS_TO_CACHE);
-        }).catch(function(error) {
-            console.log(error);
-        })
-    );
-});
-
-self.addEventListener("activate", event => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames
-          .filter(name => name.includes("dobrywebdev") && name !== CACHE_NAME)
-          .map(name => caches.delete(name))
-      )
-    }).then(() => self.clients.claim())
+    caches.open(CACHE_DYNAMIC_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(URLS_TO_CACHE);
+      }).catch(error => {
+        console.log(error);
+    })
   );
 });
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request)
-            .then(function(response) {
-            if (response) {
-                return response;
-            }
-            var fetchRequest = event.request.clone();
-            return fetch(fetchRequest).then(
-            function(response) {
-                if(!response || response.status !== 200 || response.type !== 'basic') {
-                    return response;
-            }
-            var responseToCache = response.clone();
-            caches.open(CACHE_NAME)
-                .then(function(cache) {
-                        cache.put(event.request, responseToCache);
-                    });
-                    return response;
-                }
-            );
-        })
+self.addEventListener('activate',  event => {
+  console.log('[Service Worker] Activating Service Worker ....', event);
+  event.waitUntil(
+    caches.keys()
+      .then(cacheNames => {
+        return Promise.all(
+        cacheNames
+        .map(key => {
+          if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+            console.log('[Service Worker] Removing old cache.', key);
+            return caches.delete(key);
+          }
+        }));
+      })
     );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        var fetchRequest = event.request.clone();
+        return fetch(fetchRequest).then(
+        response => {
+          if(!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+        var responseToCache = response.clone();
+        caches.open(CACHE_DYNAMIC_NAME)
+          .then(cache => {
+            cache.put(event.request, responseToCache);
+          });
+        return response;
+        }
+      );
+    })
+  );
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,155 +1,164 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
-var useref = require('gulp-useref');
-var uglify = require('gulp-uglify');
-var gulpIf = require('gulp-if');
-var imagemin = require('gulp-imagemin');
-var del = require('del');
-var cache = require('gulp-cache');
-var sequence = require('run-sequence');
-var autoprefixer = require('gulp-autoprefixer');
-var cssmin = require('gulp-cssmin');
-var cachebust = require('gulp-cache-bust');
-var replace = require('gulp-replace');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+const useref = require('gulp-useref');
+const minify = require("gulp-babel-minify");
+const gulpIf = require('gulp-if');
+const imagemin = require('gulp-imagemin');
+const del = require('del');
+const cache = require('gulp-cache');
+const sequence = require('run-sequence');
+const autoprefixer = require('gulp-autoprefixer');
+const cssmin = require('gulp-cssmin');
+const babel = require('gulp-babel');
+const babelCore = require('gulp-core');
+const cachebust = require('gulp-cache-bust');
+const replace = require('gulp-replace');
 
 gulp.task('sass', function() {
-    return gulp.src('app/scss/**/*.scss')
-        .pipe(sass())
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions', 'IE 10-11',],
-            cascade: false
-        }))
-        .pipe(gulp.dest('app/css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
-});
-
-gulp.task('watch', ['browserSync', 'sass'], function() {
-    gulp.watch('app/scss/**/*.scss', ['sass']);
-    gulp.watch('app/*.html', browserSync.reload);
-    gulp.watch('app/js/**/*.js', browserSync.reload);
-});
-
-gulp.task('browserSync', function() {
-    browserSync.init({
-        server: {
-            baseDir: 'app'
-        },
+  return gulp.src('app/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions', 'IE 10-11',],
+        cascade: false
+      }))
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
     })
+  )
 });
 
-gulp.task('useref', function(){
-    return gulp.src('app/*.html')
-        .pipe(useref())
-        .pipe(gulp.dest('dist'))
+gulp.task('watch', ['browserSync', 'sass'], () => {
+  gulp.watch('app/scss/**/*.scss', ['sass']);
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/js/**/*.js', browserSync.reload);
 });
 
+gulp.task('browserSync', () =>
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+);
+
+gulp.task('useref', () =>
+  gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(gulp.dest('dist'))
+);
 
 gulp.task('cachebust', function(){
-    return gulp.src('dist/*.html')
-        .pipe(cachebust({
-            type: 'timestamp'
-        }))
-        .pipe(gulp.dest('dist'))
+  return gulp.src('dist/*.html')
+    .pipe(cachebust({
+      type: 'timestamp'
+    }))
+    .pipe(gulp.dest('dist'))
 });
 
-gulp.task('uglify', function() {
-    return gulp.src('dist/js/main.min.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/js'))
-})
+gulp.task('babel', () =>
+  gulp.src('dist/js/main.min.js')
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(gulp.dest('dist/js'))
+);
 
-gulp.task('cssmin', function() {
-    return gulp.src('dist/css/main.min.css')
-        .pipe(cssmin())
-        .pipe(gulp.dest('dist/css'))
-})
+gulp.task("minify", () =>
+  gulp.src("dist/js/main.min.js")
+    .pipe(minify({
+      mangle: {
+        keepClassName: true
+      }
+    }))
+    .pipe(gulp.dest("dist/js"))
+);
 
-gulp.task('img', function(){
-    return gulp.src('app/img/**/*.+(png|jpg|gif|svg)')
-        .pipe(cache(imagemin()))
-        .pipe(gulp.dest('dist/img'))
+gulp.task('cssmin', () =>
+  gulp.src('dist/css/main.min.css')
+    .pipe(cssmin())
+    .pipe(gulp.dest('dist/css'))
+);
+
+gulp.task('img', () =>
+  gulp.src('app/img/**/*.+(png|jpg|gif|svg)')
+    .pipe(cache(imagemin()))
+    .pipe(gulp.dest('dist/img'))
+);
+
+gulp.task('font', () =>
+  gulp.src('app/font/**/*')
+    .pipe(gulp.dest('dist/font'))
+);
+
+gulp.task('fonts', () =>
+  gulp.src('app/fonts/**/*')
+    .pipe(gulp.dest('dist/fonts'))
+);
+
+gulp.task('php', () =>
+  gulp.src('app/phpmailer/**/*')
+    .pipe(gulp.dest('dist/phpmailer'))
+);
+
+gulp.task('form', () =>
+  gulp.src('app/formularz.php')
+    .pipe(gulp.dest('dist'))
+);
+
+gulp.task('json', () =>
+  gulp.src('app/json/**/*.json')
+    .pipe(gulp.dest('dist/json'))
+);
+
+gulp.task('favicons', () =>
+  gulp.src('app/*.png')
+    .pipe(gulp.dest('dist'))
+);
+
+gulp.task('ico', () =>
+  gulp.src('app/*.ico')
+    .pipe(gulp.dest('dist'))
+);
+
+gulp.task('xml', () =>
+  gulp.src('app/*.xml')
+    .pipe(gulp.dest('dist'))
+);
+
+gulp.task('favicon-svg', () =>
+  gulp.src('app/*.svg')
+    .pipe(gulp.dest('dist'))
+);
+
+gulp.task('manifest', () =>
+  gulp.src('app/manifest.json')
+    .pipe(gulp.dest('dist'))
+);
+
+gulp.task('serviceWorker', () =>
+  gulp.src('app/serviceworker.js')
+    .pipe(gulp.dest('dist'))
+);
+
+gulp.task('clean:dist', () =>
+  del.sync('dist')
+);
+
+gulp.task('serviceWorkerCache', () => {
+  gulp.src(['dist/serviceworker.js'])
+    .pipe(replace(/(dobrywebdev-\d{0,13})/g, (match, p1, offset, string) => {
+      let currentDate = Date.now();
+      return 'dobrywebdev-' + currentDate;
+    }))
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task('font', function(){
-    return gulp.src('app/font/**/*')
-        .pipe(gulp.dest('dist/font'))
-});
+gulp.task('default', () =>
+  sequence(['sass', 'browserSync', 'watch'])
+);
 
-gulp.task('fonts', function(){
-    return gulp.src('app/fonts/**/*')
-        .pipe(gulp.dest('dist/fonts'))
-});
-
-gulp.task('php', function(){
-    return gulp.src('app/phpmailer/**/*')
-        .pipe(gulp.dest('dist/phpmailer'))
-});
-
-gulp.task('errors', function(){
-    return gulp.src('app/errors/**/*')
-        .pipe(gulp.dest('dist/errors'))
-});
-
-gulp.task('form', function(){
-    return gulp.src('app/formularz.php')
-        .pipe(gulp.dest('dist'))
-});
-
-gulp.task('json', function(){
-    return gulp.src('app/json/**/*.json')
-        .pipe(gulp.dest('dist/json'))
-});
-
-gulp.task('favicons', function(){
-    return gulp.src('app/*.png')
-        .pipe(gulp.dest('dist'))
-});
-
-gulp.task('ico', function(){
-    return gulp.src('app/*.ico')
-        .pipe(gulp.dest('dist'))
-});
-
-gulp.task('xml', function(){
-    return gulp.src('app/*.xml')
-        .pipe(gulp.dest('dist'))
-});
-
-gulp.task('favicon-svg', function(){
-    return gulp.src('app/*.svg')
-        .pipe(gulp.dest('dist'))
-});
-
-gulp.task('manifest', function(){
-    return gulp.src('app/manifest.json')
-        .pipe(gulp.dest('dist'))
-});
-
-gulp.task('serviceWorker', function(){
-    return gulp.src('app/serviceworker.js')
-        .pipe(gulp.dest('dist'))
-});
-
-gulp.task('clean:dist', function(){
-    return del.sync('dist');
-});
-
-gulp.task('serviceWorkerCache', function(){
-    gulp.src(['dist/serviceworker.js'])
-      .pipe(replace(/(dobrywebdev-\d{0,13})/g, function(match, p1, offset, string) {
-        var currentDate = Date.now();
-        return 'dobrywebdev-' + currentDate;
-      }))
-      .pipe(gulp.dest('dist'));
-  });
-
-gulp.task('default', function(){
-    sequence(['sass', 'browserSync', 'watch'])
-});
-
-gulp.task('build', function(){
-    sequence('clean:dist', ['sass', 'useref', 'img', 'font', 'fonts', 'php', 'errors', 'json', 'form', 'favicons', 'xml', 'favicon-svg', 'manifest', 'ico', 'serviceWorker'], 'uglify', 'cssmin', 'cachebust', 'serviceWorkerCache')
-});
+gulp.task('build', () =>
+  sequence('clean:dist', ['sass', 'useref', 'img', 'font', 'fonts', 'php', 'json', 'form', 'favicons', 'xml', 'favicon-svg', 'manifest', 'ico', 'serviceWorker'], 'babel', 'minify', 'cssmin', 'cachebust', 'serviceWorkerCache')
+);
